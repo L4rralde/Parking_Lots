f5be0e655db6a4b976b80505ca70dff60f1c8c12
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2025-03-15 16:20:47 -0600

    Adding script to update data.csv on github

---
+++ src/push_data.py
@@ -0,0 +1,26 @@
+"""
+Pushes most recent version of data.csv to github
+Author: Emmanuel Larralde
+"""
+
+from datetime import datetime
+
+from misc import git_repo
+
+
+def main():
+    """
+    Pusehes new data to update_data branch on GitHub
+    """
+    git_repo.index.add('data/data.csv')
+    git_repo.index.commit(f"Updating csv file at {datetime.now()}")
+    if "update_data" not in git_repo.references:
+        git_repo.git.checkout("-b", "update_data")
+    else:
+        git_repo.git.checkout("update_data")
+    git_repo.git.push("--set-upstream", "origin", "update_data")
+    git_repo.git.checkout("main")
+
+
+if __name__ == '__main__':
+    main()
