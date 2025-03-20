03f94459b4f26abf4c3f4c0d2e7d5f259ff774f9
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2025-03-20 15:33:00 -0600

    Using another branch to update data

--- src/request_and_update.py
+++ src/request_and_update.py
@@ -8,6 +8,9 @@ from data import Data
 from misc import git_repo, now
 
 
+dst_branch = "update_data_with_actions"
+
+
 def main():
     """
     Main script. Same description as file docstring
@@ -19,18 +22,19 @@ def main():
     db.finish()
 
     when = now()
+    branch = git_repo.active_branch.name
     git_repo.index.add('data/data.csv')
     git_repo.index.commit(f"Updating csv file at {when}")
     git_repo.git.stash('save')
-    if "update_data" not in git_repo.references:
-        git_repo.git.checkout("-b", "update_data")
+    if dst_branch not in git_repo.references:
+        git_repo.git.checkout("-b", dst_branch)
     else:
-        git_repo.git.checkout("update_data")
-    git_repo.git.checkout("main", "data/data.csv")
+        git_repo.git.checkout(dst_branch)
+    git_repo.git.checkout(branch, "data/data.csv")
     git_repo.index.add('data/data.csv')
     git_repo.index.commit(f"Updating csv file at {when}")
-    git_repo.git.push("--set-upstream", "origin", "update_data")
-    git_repo.git.checkout("main")
+    git_repo.git.push("--set-upstream", "origin", dst_branch)
+    git_repo.git.checkout(branch)
     git_repo.head.reset('HEAD~1', index=True, working_tree=True)
     if git_repo.git.stash('list'):
         git_repo.git.stash('pop')
